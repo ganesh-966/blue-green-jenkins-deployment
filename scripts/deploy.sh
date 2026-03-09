6b297037a4d911a222e49f591c281d13abd30483
ganesh-ggg <ggade636@gmail.com>
2026-03-09 13:13:18 +0530

    update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+set -e
 
 SERVER_IP=$1
+KEY=/var/lib/jenkins/bluegreen.pem
 
 echo "Deploying application to $SERVER_IP"
 
-scp -o StrictHostKeyChecking=no -r app/* ubuntu@$SERVER_IP:/var/www/html/
+scp -i $KEY -o StrictHostKeyChecking=no app/index.html ubuntu@$SERVER_IP:/tmp/
 
-ssh ubuntu@$SERVER_IP << EOF
+ssh -i $KEY -o StrictHostKeyChecking=no ubuntu@$SERVER_IP << EOF
+sudo mv /tmp/index.html /var/www/html/index.html
 sudo systemctl restart nginx
 EOF
 
